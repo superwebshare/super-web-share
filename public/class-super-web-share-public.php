47393716782f7ae999b279a194fe3ddbe7ac53db
Jose Varghese <34977009+josevarghese@users.noreply.github.com>
2019-12-31 15:26:47 +0530

    moved all add actions calls to constructor

--- public/class-super-web-share-public.php
+++ public/class-super-web-share-public.php
@@ -41,6 +41,23 @@ class Super_Web_Share_Public {
 	public function __construct( $plugin_name, $version ) {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
+		add_action('wp_footer', 'superwebshare_floating_button_code');
+		add_action('wp_footer', 'superwebshare_frontent_inline_styles');
+		add_action('the_content', 'superwebshare_normal_button_code');
+	}
+	
+	
+		/**
+	 * Hook the plugin function on 'init' event.
+	 *
+	 * @since    1.0.0
+	 */
+public function init() {
+		if ( function_exists( 'is_amp_endpoint' ) ) {
+			add_action( 'wp_print_styles', 'frontend_amp_css' );
+        	add_action( 'amp_post_template_css', 'frontend_amp_css' );
+    	}
+	
 	}
 	/**
 	 * Register the stylesheets for the public-facing side of the site.
@@ -48,7 +65,7 @@ class Super_Web_Share_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-		if ( ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) && ! ( function_exists( 'ampforwp_is_amp_endpoint' ) && ampforwp_is_amp_endpoint() ) ) {
+		
 			$settings_floating = superwebshare_get_settings_floating();
 			$settings = superwebshare_get_settings();
 			if (( $settings_floating['superwebshare_floating_enable'] == 'enable') || ( $settings['superwebshare_normal_enable'] == 'enable')) {
@@ -62,7 +79,6 @@ class Super_Web_Share_Public {
 						wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/super-web-share-public.css', array(), $this->version, 'all' );
 					}
 			}
-		}
 	}
 	/**
 	 * Register the JavaScript for the public-facing side of the site.
@@ -70,7 +86,6 @@ class Super_Web_Share_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-		if ( ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) && ! ( function_exists( 'ampforwp_is_amp_endpoint' ) && ampforwp_is_amp_endpoint() ) ) {
 			$settings_floating = superwebshare_get_settings_floating();
 			$settings = superwebshare_get_settings();
 			if (( $settings_floating['superwebshare_floating_enable'] == 'enable') || ( $settings['superwebshare_normal_enable'] == 'enable')) {
@@ -85,14 +100,13 @@ class Super_Web_Share_Public {
 					}
 				}
 			}
-		}
-	}
+
 /**
 	 * Floating Share Button
 	 *
 	 * @since 1.0
 */
-function superwebshare_floating_button_code() {
+	function superwebshare_floating_button_code() {
 	if ( ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) && ! ( function_exists( 'ampforwp_is_amp_endpoint' ) && ampforwp_is_amp_endpoint() ) ) {
 		$settings_floating = superwebshare_get_settings_floating();
 		if ( $settings_floating['superwebshare_floating_enable'] == 'enable') {
@@ -102,13 +116,13 @@ function superwebshare_floating_button_code() {
 				|| ( isset($settings_floating['floating_display_home']) == '1' && is_home() ) ) {
 		
 					$tags  = '<!-- Floating Button by SuperWebShare - Native Share Plugin for WordPress -->' . PHP_EOL;
-    				echo '<div class="sws_superaction"><button class="superwebshare_prompt superwebshare_tada superwebshare_button" style="background-color: '. $settings_floating['floating_share_color'] .'; '.$settings_floating['floating_position'].':'.$settings_floating['floating_position_leftright'].'px; bottom:'.$settings_floating['floating_position_bottom'].'px;";><span> Share </span></button></div>' . PHP_EOL;
+    				echo '<div id="superwebshare-id" class="hidden sws_superaction" onclick="triggerNativeShare()"><button class="superwebshare_prompt superwebshare_tada superwebshare_button" style="background-color: '. $settings_floating['floating_share_color'] .'; '.$settings_floating['floating_position'].':'.$settings_floating['floating_position_leftright'].'px; bottom:'.$settings_floating['floating_position_bottom'].'px;";><span> Share </span></button></div>' . PHP_EOL;
 					echo $tags;
 				}
 			}
 		}
 	}
-add_action('wp_footer', 'superwebshare_floating_button_code');
+
 /**
 	 * Normal Share Button
 	 *
@@ -117,38 +131,38 @@ add_action('wp_footer', 'superwebshare_floating_button_code');
 	 * @return string Modifiyed content
 	 * @since 1.2
 */
-function superwebshare_normal_button_code($content) {
-	if ( ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) && ! ( function_exists( 'ampforwp_is_amp_endpoint' ) && ampforwp_is_amp_endpoint() ) ) {
-		$settings = superwebshare_get_settings();
-		if ( $settings['superwebshare_normal_enable'] == 'enable') {
-			if ( is_single()
-				|| ( isset($settings['normal_display_page']) == '1' && is_page() )
-				|| ( isset($settings['normal_display_archive']) == '1' && is_archive() )
-				|| ( isset($settings['normal_display_home']) == '1' && is_home() ) ) {
-			$pos = $settings['position'];
-			$button = '<div class="sws_supernormalaction"><button class="superwebshare_normal_button1 superwebshare_prompt" style="background-color: '. $settings['normal_share_color'] .';";>'.'<span>'. $settings['normal_share_button_text'] .'</span></button></div>';
-			switch ( $pos ) {
-				case 'before':
-					$content = $button . $content;
-					break;
-				case 'after':
-					$content = $content . $button;
-					break;
-				case 'both':
-					$content = $button . $content . $button;
-					break;
-				case 'manual':
-					break;
-				default:
-					$content = $content . $button;
-					break;
+	function superwebshare_normal_button_code($content) {
+		if ( ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) && ! ( function_exists( 'ampforwp_is_amp_endpoint' ) && ampforwp_is_amp_endpoint() ) ) {
+			$settings = superwebshare_get_settings();
+			if ( $settings['superwebshare_normal_enable'] == 'enable') {
+				if ( is_single()
+					|| ( isset($settings['normal_display_page']) == '1' && is_page() )
+					|| ( isset($settings['normal_display_archive']) == '1' && is_archive() )
+					|| ( isset($settings['normal_display_home']) == '1' && is_home() ) ) {
+				$pos = $settings['position'];
+				$button = '<div class="sws_supernormalaction"><button class="superwebshare_normal_button1 superwebshare_prompt" style="background-color: '. $settings['normal_share_color'] .';";>'.'<span>'. $settings['normal_share_button_text'] .'</span></button></div>';
+				switch ( $pos ) {
+					case 'before':
+						$content = $button . $content;
+						break;
+					case 'after':
+						$content = $content . $button;
+						break;
+					case 'both':
+						$content = $button . $content . $button;
+						break;
+					case 'manual':
+						break;
+					default:
+						$content = $content . $button;
+						break;
+					}
 				}
 			}
 		}
+		return $content;
 	}
-	return $content;
-}
-add_action('the_content', 'superwebshare_normal_button_code');
+
 /**
 	 * Inline Styles for Share Button (Icon)
 	 *
@@ -156,9 +170,8 @@ add_action('the_content', 'superwebshare_normal_button_code');
 	 * @return string Icon for Share Button
 	 * @since 1.4.1
 */
-function superwebshare_frontent_inline_styles($content)
-{
-	if ( ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) && ! ( function_exists( 'ampforwp_is_amp_endpoint' ) && ampforwp_is_amp_endpoint() ) ) {
+	function superwebshare_frontent_inline_styles($content) {
+		if ( ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) && ! ( function_exists( 'ampforwp_is_amp_endpoint' ) && ampforwp_is_amp_endpoint() ) ) {
  		$settings_floating = superwebshare_get_settings_floating();
     	$settings = superwebshare_get_settings();
     	if (($settings_floating['superwebshare_floating_enable'] == 'enable') || ($settings['superwebshare_normal_enable'] == 'enable')) {
@@ -169,12 +182,11 @@ function superwebshare_frontent_inline_styles($content)
             	|| (isset($settings['normal_display_page']) == '1' && is_page())
             	|| (isset($settings['normal_display_archive']) == '1' && is_archive())
             	|| (isset($settings['normal_display_home']) == '1' && is_home())
-        	) {
+        		) {
             	echo '<style type="text/css"> .superwebshare_prompt::before { background-image: url(' . plugin_dir_url (__FILE__) . 'assets/android_share.svg);
                 	} 
 				</style>' . PHP_EOL;
+				}
 			}
 		}
 	}
-}
-add_action('wp_footer', 'superwebshare_frontent_inline_styles');
