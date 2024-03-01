4feef33993fcd6f48afbc71ac7a0427510cd5f74
josevarghese <trackbulls@gmail.com>
2024-03-01 21:58:32 +0530

    package update and block preview

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cb94daa010e6ed694cc9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '86105abb53bcacedb912');
--- block/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '4487f13e27c5a19523fa');
